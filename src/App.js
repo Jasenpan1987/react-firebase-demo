97ed2d4b22f80e1a02063f173249b9bd4ece7f34
Jasenpan1987 <jasenpan@gmail.com>
2018-10-01 14:29:29 +1000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
-    return <div className="App">hello world</div>;
+    return (
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
   }
 }
 
